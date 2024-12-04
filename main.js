54f7b87c929db6b556f8da5c4072ecb5836d3112
ogabek <asd@.com>
2024-12-04 11:19:36 +0500

    birinchi commit

---
+++ main.js
@@ -0,0 +1,30 @@
+
+var a = prompt('Birinchi sonni kiriting');
+var b = prompt('Ikkinchi sonni kiriting');
+var c = prompt('Uchinchi sonni kiriting');
+
+
+if (a === null || a.trim() === "" || b === null || b.trim() === "" || c === null || c.trim() === "") {
+    console.log("Siz amalni bajarmadingiz!");
+    alert("Siz amalni bajarmadingiz!");
+} else if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    console.log("Matn emas, raqam kiriting!");
+    alert("Matn emas, raqam kiriting!");
+} else {
+
+    if ((a > b && a < c) || (a > c && a < b)) {
+
+        console.log( a + " -birinchi kiritilgan son O'rta qiymat ");
+        
+    } else if ((b > a && b < c) || (b > c && b < a)) {
+
+        console.log( b + " -ikkinchi kiritilgan son O'rta qiymat ");
+    } else {
+
+        console.log( c + " -uchinchi kiritilgan son O'rta qiymat");
+    }
+
+
+}
+
+
